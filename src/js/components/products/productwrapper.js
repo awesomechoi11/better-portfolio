97c90be4bde542b138d3dd6f59dad9063e0da9cb
brandon <brandonchoi2016@gmail.com>
2021-03-03 05:00:21 -0800

    close animation on product

--- src/js/components/products/productwrapper.js
+++ src/js/components/products/productwrapper.js
@@ -1,26 +1,55 @@
 import { PreviewHeader } from '../previewcard/previewcard'
 import products from '../../../config.json'
 import '../../../sass/product.scss'
-import { fromPreviewToProduct } from '../../animVariants'
-import { openProduct_atom, selectedPreview_atom } from '../../utils/Atom'
+import { exitPreview, fromPreviewToProduct } from '../../animVariants'
+import { openProduct_atom, scrollEnabled_atom, selectedPreview_atom } from '../../utils/Atom'
 import { AnimatePresence, motion } from 'framer-motion'
-import { useRecoilState, useRecoilValue } from 'recoil'
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
 
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import PerfectScrollbar from 'react-perfect-scrollbar'
 
+const closeSvg = (
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" >
+        <path d="M1 1L31 31M1 1V31M1 1H31M31 31V1M31 31H1M31 1L1 31" stroke="#E9EDDE" />
+    </svg>
+
+)
+
+
 export default function ProductWrapper() {
 
     const [openProduct, setOpenProduct] = useRecoilState(openProduct_atom)
+    const setScrollEnabled = useSetRecoilState(scrollEnabled_atom)
     const data = products[openProduct.index]
+
+    function closeProduct() {
+        setOpenProduct({ isOpen: false, index: -1 })
+        setScrollEnabled(true)
+    }
+
     return (
         <AnimatePresence>
             {openProduct.isOpen && (
-                <motion.div className='product-wrapper'>
-                    <div className='content'>
-                        <PreviewHeader {...data} />
-                    </div>
-                    <Product />
+                <motion.div
+                    initial={false}
+                    animate='animate'
+                    exit='exit'
+                    variants={exitPreview}
+                    className='product-wrapper'>
+                    <PerfectScrollbar>
+                        <div className='util-btns'>
+                            <div className='close-product'
+                                onClick={closeProduct}
+                            >
+                                close
+                            </div>
+                        </div>
+                        <div className='content'>
+                            <PreviewHeader {...data} />
+                        </div>
+                        <Product />
+                    </PerfectScrollbar>
                 </motion.div>
             )}
         </AnimatePresence>
@@ -28,11 +57,10 @@ export default function ProductWrapper() {
 
 }
 
-console.log(products)
 
 function Product() {
     return (
-        <div>
+        <div className='product-showcase'>
             hello
         </div>
     )
